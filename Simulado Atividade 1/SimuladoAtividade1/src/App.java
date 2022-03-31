d54554f098b11518754aae4a9b6cf4b62f131198
FelipeSilvieri <felipesilvieri@yahoo.com>
2022-03-31 09:52:53 -0300

    Primeiro Commit pra salvar

---
+++ Simulado Atividade 1/SimuladoAtividade1/src/App.java
@@ -0,0 +1,8 @@
+// Nome: Felipe Matos Silvieri
+// RA: 20.00314-5
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
