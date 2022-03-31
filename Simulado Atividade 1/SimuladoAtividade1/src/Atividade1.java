f0f725044314de6d69deb02ddd1a8e04e4c32a00
FelipeSilvieri <felipesilvieri@yahoo.com>
2022-03-31 09:55:24 -0300

    jhhj

--- Simulado Atividade 1/SimuladoAtividade1/src/Atividade1.java
+++ Simulado Atividade 1/SimuladoAtividade1/src/Atividade1.java
@@ -1,7 +1,7 @@
 // Nome: Felipe Matos Silvieri
 // RA: 20.00314-5
 
-public class App {
+public class Atividade1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
