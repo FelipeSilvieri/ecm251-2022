da87e4e0976e87ff262b24b443248177984f4a35
FelipeSilvieri <felipesilvieri@yahoo.com>
2022-05-26 10:47:44 -0300

    committolass

---
+++ ECM-251 Aula12/Pilares POO/src/EnumCategoriaBebida.java
@@ -0,0 +1,3 @@
+public enum EnumCategoriaBebida {
+    SUCP,ALCOOL,AGUA,REFRIGERANTE,CHA,CAFE,LEITE,ENERGETICO;
+}
---
+++ ECM-251 Aula12/Pilares POO/src/Produto.java
@@ -0,0 +1,34 @@
+public class Produto {
+    private final double preco;
+    private final String nome;
+    private int quantidade;
+    private final String descricao;
+    
+    public Produto(double preco, String nome, int quantidade, String descricao){
+        this.preco = preco;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+    
+}
