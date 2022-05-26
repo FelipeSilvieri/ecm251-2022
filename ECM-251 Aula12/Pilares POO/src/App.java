1a7f775b280460083af7f15dd39cc3f9cedcfedc
FelipeSilvieri <felipesilvieri@yahoo.com>
2022-05-26 10:53:57 -0300

    ergre

--- ECM-251 Aula12/Pilares POO/src/App.java
+++ ECM-251 Aula12/Pilares POO/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Produto cornDog = new Comida(12.5, "Corn dog", 5, "Cachorro quente diferenciado", EnumCategoriaComida.COREANA, EnumAlergicos.GLUTEN, EnumPimenta.SUAVE);
+
+        Produto acaiMoleza = new Bebida(10, "Açai do Molezolas", 7, "Açai top maravilhoso", EnumCategoriaBebida.SUCO, EnumTemperatura.FRIO);
+
+        System.out.println("Preços Regulares: ");
+        System.out.println(cornDog.getnome()+ ":R$" + cornDog.getPreco());
+        System.out.println(acaiMoleza.getNome()+":R$" + acaiMoleza.getPreco());
+
     }
 }
