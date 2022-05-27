45b0dcc2dd27f20ed539d5d3cd5a584dc066925b
FelipeSilvieri <felipesilvieri@yahoo.com>
2022-05-27 12:32:01 -0300

    efdefe

---
+++ Lojinha/Loja/src/Produtos.java
@@ -0,0 +1,39 @@
+public abstract class Produtos {
+
+    private final String cor;
+    private final double preco;
+    private final String descricao;
+    private final String tipo;
+    private final double precofinal;
+
+
+    public Produtos(String cor, double preco, String descricao, String tipo, double precofinal) {
+        this.cor = cor;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.precofinal = precofinal;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPrecofinal() {
+        return precofinal;
+    }
+    
+    
+}
