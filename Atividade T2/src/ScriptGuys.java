cf161266cf18c4341eec0a26d644188641d0ff1c
Felipe_Silvieri <felipesilvieri@yahoo.com>
2022-06-29 10:47:07 -0300

    ScriptGuys

---
+++ Atividade T2/src/ScriptGuys.java
@@ -0,0 +1,13 @@
+public class ScriptGuys extends Integrantes{
+
+    public ScriptGuys(String nome, String email, String turno) {
+        super(nome, email, turno);
+    }
+
+    @Override
+    public boolean postagens() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
