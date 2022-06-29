646c8ae623ba5a4b4b0fee0e8272a7a01bc70080
Felipe_Silvieri <felipesilvieri@yahoo.com>
2022-06-29 12:19:00 -0300

    adicionando

--- AtividadeT2/src/SistemaPrincipal.java
+++ AtividadeT2/src/SistemaPrincipal.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 public class SistemaPrincipal {
     void run(){
         ArrayList<Integrantes> integrantes = new ArrayList<Integrantes>();
+
+        integrantes.add(new BigBrothers("Felipe", "felipe123@yahoo.com", "regular"));
+
+        integrantes.add(new HeavyLifters("Roberta", "robertaalmeida@gmail.com", "regular"));
+
+        integrantes.add(new MobileMembers("Deyverson", "deyvinho@bol.com", "regular"));
+
+        integrantes.add(new ScriptGuys("Kleber", "kle0908@hotmail.com", "regular"));
     }
     
 }
