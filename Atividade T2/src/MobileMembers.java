ccf698cef694d880c0cfac8c5d72e64ed9586ed8
Felipe_Silvieri <felipesilvieri@yahoo.com>
2022-06-29 10:47:56 -0300

    MobileMembers

---
+++ Atividade T2/src/MobileMembers.java
@@ -0,0 +1,13 @@
+public class MobileMembers extends Integrantes{
+
+    public MobileMembers(String nome, String email, String turno) {
+        super(nome, email, turno);
+    }
+
+    @Override
+    public boolean postagens() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
