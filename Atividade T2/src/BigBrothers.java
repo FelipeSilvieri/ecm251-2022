20681c1ed1a518b1c81ef074fe1ce6c6d026b9ec
Felipe_Silvieri <felipesilvieri@yahoo.com>
2022-06-29 10:40:37 -0300

    Big Brothers

---
+++ Atividade T2/src/BigBrothers.java
@@ -0,0 +1,14 @@
+public class BigBrothers extends Integrantes{
+
+    public BigBrothers(String nome, String email, String turno) {
+        super(nome, email, turno);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean postagens() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
